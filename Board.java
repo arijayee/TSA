f2f789e1a3163cc4a52ef8265a48687498da78dc
arijayee <111769589+arijayee@users.noreply.github.com>
2022-11-14 14:51:00 -0700

    Add files via upload
    Here is the outline that I have been working out for the video game today :) 
    Heres the website I used to help me: https://zetcode.com/javagames/basics/

---
+++ Board.java
@@ -0,0 +1,15 @@
+package com.zetcode;
+import java.awt.Color;
+import java awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2d;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JPanel;
+
+public class Board extends Jpanel {
+    
+    public Board() {}
+
+}
